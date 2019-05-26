722cf02c445df922b97951056425d7f5b8c5ae87
Minhaz Rafi Chowdhury <minhazr@gmail.com>
2019-05-26 10:07:52 -0700

    fix error in downloaded project
    updating package names to match androidx

--- app/src/main/java/com/jobrapp/androidinterview/MainActivity.kt
+++ app/src/main/java/com/jobrapp/androidinterview/MainActivity.kt
@@ -1,14 +1,7 @@
 package com.jobrapp.androidinterview
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
-import com.jobrapp.server.Server
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CommonPool
-import kotlinx.coroutines.android.UI
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
