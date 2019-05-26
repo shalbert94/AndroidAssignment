93ca45724ed38242181106a30df3453a79c78cad
Minhaz Rafi Chowdhury <minhazr@gmail.com>
2019-05-26 14:40:29 -0700

    loading status indicator

--- app/src/main/java/com/jobrapp/androidinterview/MainActivity.kt
+++ app/src/main/java/com/jobrapp/androidinterview/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.jobrapp.androidinterview
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
@@ -12,14 +15,19 @@ import com.jobrapp.androidinterview.util.AutoFitGridLayoutManager
 import com.jobrapp.androidinterview.util.EndlessScrollListener
 import com.jobrapp.androidinterview.util.GridDecoration
 import com.jobrapp.server.User
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var model: JobViewModel
     private var jobAdapter:JobAdapter?=null;
+    private var progressBar: ProgressBar?=null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tolbar=findViewById<Toolbar>(R.id.toolbar)
+        tolbar.setTitle(R.string.app_name)
+        toolbar.setTitleTextColor(Color.WHITE)
         model = ViewModelProviders.of(this).get(JobViewModel::class.java)
         val dataObserver = Observer<Response<List<User>>> {
             result->updateUi(result)
@@ -36,9 +44,12 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager= layoutManager
         recyclerView.adapter=jobAdapter
         DataRepository.getInstance().loadData()
+        progressBar=findViewById(R.id.progressBar)
+        progressBar!!.visibility= View.VISIBLE
         recyclerView.addOnScrollListener(object: EndlessScrollListener(layoutManager){
             override fun loadBottom() {
                 DataRepository.getInstance().loadData()
+                progressBar!!.visibility= View.VISIBLE
             }
 
             override fun loadTop() {
@@ -49,6 +60,7 @@ class MainActivity : AppCompatActivity() {
 
     fun updateUi(result:Response<List<User>>?){
         if (result!!.isSuccessful){
+            progressBar!!.visibility= View.GONE
             jobAdapter?.addAll(result.body())
             Log.d("MainActivity", result.toString())
 
